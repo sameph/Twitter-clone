8b05b5e69eb42f72b6d46848f0915ce697489a05
sameph <samuelephrem2012@gmail.com>
2024-12-04 18:06:22 +0300

    project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup = async (req, res) =>{
+  res.json({
+    data: 'You hit the signup endpoint 👍',
+  })
+}
+export const login = async (req, res) =>{
+  res.json({
+    data: 'You hit the login endpoint 👍',
+  })
+}
+export const logout = async (req, res) =>{
+  res.json({
+    data: 'You hit the logout endpoint 👍',
+  })
+}
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import authRoutes from './routes/auth.routes.js';
+import connectMongoDB from './db/connectMongoDB.js';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT | 5000;
+
+// console.log(process.env.MONGO_URI);
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, ()=>{
+  console.log(`Server started on http://localhost:${PORT}`);
+  connectMongoDB();
+})
