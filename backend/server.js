2fb2ec0b827f42ea0634bf24e17e94546f937036
sameph <samuelephrem2012@gmail.com>
2024-12-05 23:11:54 +0300

    auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,23 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 
-import authRoutes from './routes/auth.routes.js';
-import connectMongoDB from './db/connectMongoDB.js';
+import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT | 5000;
 
 // console.log(process.env.MONGO_URI);
+app.use(express.json()); // for parsing areq.body
 
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, ()=>{
+
+app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
   connectMongoDB();
-})
+});
