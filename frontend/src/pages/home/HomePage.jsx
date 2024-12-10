8286059fd5fa45c762f64d3ef3f9e46e33ffde3b
sameph <samuelephrem2012@gmail.com>
2024-12-10 23:47:33 +0300

    completed the the frontend

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
+import Posts from "../../components/common/Posts";
 import CreatePost from "./CreatePost";
-import Posts from "../../../components/common/Posts";
 
 const HomePage = () => {
 	const [feedType, setFeedType] = useState("forYou");
@@ -37,7 +37,7 @@ const HomePage = () => {
 				<CreatePost />
 
 				{/* POSTS */}
-				<Posts />
+				<Posts feedType={feedType} />
 			</div>
 		</>
 	);
